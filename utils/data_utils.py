0efc1df271b0a35f6bec4ef7e5fe9b4ce97aa995
brian-dev <6363947+brian-dev@users.noreply.github.com>
2023-09-16 08:52:54 -0500

    fix file path

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -31,5 +31,5 @@ def generate_user_payload():
 
 def fetch_endpoints():
     with open('yaml/endpoints.yaml') as file_read:
-        file = yaml.load(file_read, Loader=yaml.FullLoader)
+        file = yaml.safe_load(file_read)
     return file['endpoints']
